9e983e38b76ff68888647b463d2dbc8a274cd8bc
ClaudiaGarciaGS <claudia.ggutierrez@cunef.edu>
2025-01-10 16:33:06 +0100

    Tarea.cpp

--- Tarea.cpp
+++ Tarea.cpp
@@ -1,9 +1,14 @@
 #include "Tarea.hpp"
 #include <iostream>
+
 using namespace std;
 
+// Desarrollo de los métodos
+
+// Constructor
 Tarea::Tarea(const string &t, const string &f, const string &d) : Actividad(t), fechaLimite(f), descripcion(d) {}
 
+// Redefinimos el método que era virtual, muestra la información de la tarea
 void Tarea::mostrar() const {
     cout << "Titulo: " << get_titulo()
          << "\nCompletada: " << (get_estaCompletada() ? "SI" : "NO")
@@ -12,18 +17,18 @@ void Tarea::mostrar() const {
          << endl;
 }
 
+// Getters y setters de fecha limite
 string Tarea::get_fechaLimite() const {
     return fechaLimite;
 }
-
 void Tarea::set_fechaLimite(const string &f) {
     fechaLimite = f;
 }
 
+// Getters y setters de descripción
 string Tarea::get_descripcion() const {
     return descripcion;
 }
-
 void Tarea::set_descripcion(const string &d) {
     descripcion = d;
 }
