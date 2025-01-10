9c1539e1f92c7a4d3ea9127ca7fc7233e7c2e1f0
ClaudiaGarciaGS <claudia.ggutierrez@cunef.edu>
2025-01-10 16:23:25 +0100

    Actividad.hpp

--- Actividad.hpp
+++ Actividad.hpp
@@ -2,20 +2,21 @@
 #define ACTIVIDAD_HPP
 
 #include <string>
+
 using namespace std;
 
+// Creamos una clase abstracta de la que van a derivar las tareas
 class Actividad {
 protected:
     string titulo;
     bool estaCompletada;
 
 public:
+    // Declaración de los métodos que vamos a desarrollar
     Actividad(const string &tit);
     virtual ~Actividad();
-
     virtual void mostrar() const = 0;
     void completar();
-
     string get_titulo() const;
     void set_titulo(const string &t);
     bool get_estaCompletada() const;
