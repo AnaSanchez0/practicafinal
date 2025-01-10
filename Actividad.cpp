05fb065211c10c7b6d80e364d4379ba51b7300a1
ClaudiaGarciaGS <claudia.ggutierrez@cunef.edu>
2025-01-10 16:28:17 +0100

    Actividad.cpp

--- Actividad.cpp
+++ Actividad.cpp
@@ -1,25 +1,30 @@
 #include "Actividad.hpp"
 
+// Desarrollo de los métodos
+
+// Constructor, inicializamos como no completada
 Actividad::Actividad(const string &tit) : titulo(tit), estaCompletada(false) {}
 
+// Destrucutor virtual para eliminar subclases
 Actividad::~Actividad() {}
 
+// Método que marca la actividad como completada
 void Actividad::completar() {
     estaCompletada = true;
 }
 
+ // Getters y setters de tíulo
 string Actividad::get_titulo() const {
     return titulo;
 }
-
 void Actividad::set_titulo(const string &t) {
     titulo = t;
 }
 
+// Getters y setters de estaCompletada
 bool Actividad::get_estaCompletada() const {
     return estaCompletada;
 }
-
 void Actividad::set_estaCompletada(bool estado) {
     estaCompletada = estado;
 }
