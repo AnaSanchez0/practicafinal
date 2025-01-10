127fe35f00188eb42aff1907b55a4ec4b5664e9b
ClaudiaGarciaGS <claudia.ggutierrez@cunef.edu>
2025-01-10 16:11:16 +0100

    ListaDeTareas.hpp

--- ListaDeTareas.hpp
+++ ListaDeTareas.hpp
@@ -3,19 +3,21 @@
 
 #include "Tarea.hpp"
 #include <vector>
+#include <string>
 
 class ListaDeTareas {
 private:
+    // Creamos una clase vector, para agrupar las tareas en una lista
     vector<Tarea*> tareas;
 
 public:
-    ~ListaDeTareas();
-
+    // Declaración de los métodos que vamos a desarrollar
     void agregarTarea(Tarea* tarea);
     void eliminarTarea(const string &titulo);
     void mostrarTareas() const;
     void cargarDesdeArchivo(const string &nombreArchivo);
     void guardarEnArchivo(const string &nombreArchivo) const;
+    ~ListaDeTareas();
 };
 
 #endif
