15ce450d7a5013abd18eb18aec3740c806d05aec
ClaudiaGarciaGS <claudia.ggutierrez@cunef.edu>
2025-01-10 16:31:01 +0100

    Tarea.hpp

--- Tarea.hpp
+++ Tarea.hpp
@@ -2,16 +2,18 @@
 #define TAREA_HPP
 
 #include "Actividad.hpp"
+#include <string>
 
+// Clase para las tareas, herencia de Actividad
 class Tarea : public Actividad {
 private:
     string fechaLimite;
     string descripcion;
 
 public:
+    // Declaración de los métodos que vamos a desarrollar
     Tarea(const string &t, const string &f, const string &d);
     void mostrar() const override;
-
     string get_fechaLimite() const;
     void set_fechaLimite(const string &f);
     string get_descripcion() const;
