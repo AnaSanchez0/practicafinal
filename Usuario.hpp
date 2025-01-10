4f41b2013b1e6dc4d7f416492e49a2506e4d2baf
ClaudiaGarciaGS <claudia.ggutierrez@cunef.edu>
2025-01-10 16:35:08 +0100

    Usuario.hpp

--- Usuario.hpp
+++ Usuario.hpp
@@ -2,7 +2,9 @@
 #define USUARIO_HPP
 
 #include "ListaDeTareas.hpp"
+#include <string>
 
+// Clase para el usuario
 class Usuario {
 private:
     string nombre;
@@ -11,11 +13,10 @@ private:
     ListaDeTareas listaDeTareas;
 
 public:
+    // Declaración de los métodos que vamos a desarrollar
     Usuario(const string &nom, const string &em, const string &pass);
-
     ListaDeTareas& obtenerListaDeTareas();
     bool iniciarSesion(const string &em, const string &pass) const;
-
     string get_nombre() const;
     void set_nombre(const string &nom);
     string get_email() const;
